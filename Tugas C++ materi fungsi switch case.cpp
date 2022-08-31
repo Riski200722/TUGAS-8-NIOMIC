fcc70e6ba3a140df4eadc998aea90c3873ba4c9f
RISKI <Riski180722@gmail.com>
2022-08-31 22:54:06 +0700

    Tugas C++ materi fungsi switch case

---
+++ Tugas C++ materi fungsi switch case.cpp
@@ -0,0 +1,34 @@
+ #include<iostream>
+using namespace std;
+int main(){
+    int X;
+    
+    X = 90000;
+
+    switch (X){
+        case 10000:
+        cout<<"Sepuluh ribu rupiah";
+        break;
+        case 20000:
+        cout<<"Dua puluh ribu rupiah";
+        break;
+        case 30000:
+        cout<<"Tiga puluh ribu rupiah";
+        break;
+        case 40000:
+        cout<<"Empat puluh ribu rupiah";
+        break;
+        case 50000:
+        cout<<"Lima puluh ribu rupiah";
+        break;
+        default: 
+        cout<<"Tidak Terdaftar di program";
+    }
+    
+    return 0;
+}
+
+
+
+
+
